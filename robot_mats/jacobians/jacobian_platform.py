c232f1bed1bbe26889112275deddbe0e7f4510ea
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-09-14 14:51:21 +0200

    added jacobian in x,y,theta space

--- robot_mats/jacobians/jacobian_platform.py
+++ robot_mats/jacobians/jacobian_platform.py
@@ -4,9 +4,29 @@ from math import sin, cos, pi
 import numpy as np
 
 def getJacobianPlatform():
+    return getJacobianPlatformWithoutEEF()
+
+def getJacobianPlatformWithoutEEF():
+    """get Jacobian of platform with coordinates x,theta
+
+    Returns:
+        Jacobian (np.array((6,2)))
+    """
     J = np.zeros((6, 2))
     J[0,0]=1
     J[5,1]=1
+    return J
+
+def getJacobianPlatformWithEEF():
+    """get Jacobian of platform with coordinates x,y,theta
+
+    Returns:
+        Jacobian (np.array((6,3)))
+    """
+    J = np.zeros((6, 3))
+    J[0,0]=1
+    J[1,1]=1
+    J[5,2]=1
     return J
 
 def getRotationMatrix(theta):
@@ -19,10 +39,18 @@ def getRotationMatrix(theta):
     return R
 
 def getRotationMatrixS3(theta):
-    G = np.zeros((6, 6))
-    G[0:3,0:3]=getRotationMatrix(theta)
-    G[3:6,3:6]=getRotationMatrix(theta)
-    return G
+    """gets rotation matrix in S3
+
+    Args:
+        theta (float): robot direction
+
+    Returns:
+        np.array(6,6): Rotation matrix for S3 (R3 and SO3)
+    """
+    R6 = np.zeros((6, 6))
+    R6[0:3,0:3]=getRotationMatrix(theta)
+    R6[3:6,3:6]=getRotationMatrix(theta)
+    return R6
 
 def getJacobianPlatformToMap(theta):
     # J = np.zeros((6, 2))

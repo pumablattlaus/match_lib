3e2c4d63e9ced652540c13a09bf346122b057f13
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-11-19 18:48:50 +0100

    get velocities from topic

--- import_bag.py
+++ import_bag.py
@@ -45,21 +45,39 @@ def get_all_trees_from_bag(bag_file_name: str, frame_names: List[str], set_known
                 set_known_frames(tree)
 
     return pose_time, pose_hand_fin
-    
 
-if __name__ == "__main__":
-    path="/home/hauke/Documents/WindowsDocuments/"
-    name = "2022-11-14-15-26-03.bag"
 
+def mur_write_all_trees_from_bag(bag_file_name: str):
     frame_names = ["robot_pose", "pose_to_base_link_inertia",
-                    "mur/ur/shoulder_link", 'mur/ur/upper_arm_link', 'mur/ur/forearm_link', 'mur/ur/wrist_1_link', 'mur/ur/wrist_2_link', 'mur/ur/wrist_3_link']
+                "mur/ur/shoulder_link", 'mur/ur/upper_arm_link', 'mur/ur/forearm_link', 'mur/ur/wrist_1_link', 'mur/ur/wrist_2_link', 'mur/ur/wrist_3_link']
 
     pose_to_base_link_inertia = MyPose((-0.350, 0.150, -0.350), (0.000, 0.000, 0.000, 1.000))
     def set_known_frames(tree):
         tree[1]=pose_to_base_link_inertia
         return tree
                 
-    pose_time, pose_hand_fin = get_all_trees_from_bag(path+name, frame_names, set_known_frames)
-    with open(path + name+'.csv','w') as fd:
+    pose_time, pose_hand_fin = get_all_trees_from_bag(bag_file_name, frame_names, set_known_frames)
+    with open(bag_file_name+'.csv','w') as fd:
         for t, pose in zip(pose_time, pose_hand_fin):
             fd.write(t.__str__()+";"+pose.position.__reduce__()[2].__str__()[1:-1] + ";" + pose.orientation.__reduce__()[2].__str__()[1:-1]+"\n")
+
+def write_twists(bag_file_name, topic_name: str = "/mur/velocity_command"):
+    twists = []
+    times = []
+    for topic, msg, t in rosbag.Bag(bag_file_name).read_messages(topics=[topic_name]):
+        times.append(t)
+        twists.append(msg)
+    with open(bag_file_name+'_cmds.csv','w') as fd:
+        for t, cmd in zip(times, twists):
+            fd.write(t.__str__()+";"+cmd.linear.__reduce__()[2].__str__()[1:-1] + ";" + cmd.angular.__reduce__()[2].__str__()[1:-1]+"\n")
+
+
+if __name__ == "__main__":
+    path="/home/hauke/Documents/WindowsDocuments/"
+    name = "2022-11-14-15-26-03.bag"
+    bag_file_name=path+name
+
+    # mur_write_all_trees_from_bag(bag_file_name)
+    # write_twists(bag_file_name)
+
+    

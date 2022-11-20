dc68d205e3d8984b2dd874b0eb78ae908b7f1a0e
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-11-20 12:25:57 +0100

    listening to tf_pose_topics

--- topics.py
+++ topics.py
@@ -6,22 +6,36 @@ from match_geometry import MyPose
 
 if __name__ == '__main__':
     target_frame = 'mur/ur/wrist_3_link'
-    source_frame = 'map'
+    source_frame = 'mur/ur/base_link'
+    
+    source_frame_mir = "mur/mir/base_footprint"
+    target_frame_mir = "map"
+
+    # source_frame = 'map'
     rospy.init_node('tf_listener')
 
     listener = tf.TransformListener()
 
     pub = rospy.Publisher('tf_pose', geometry_msgs.msg.Pose,queue_size=1)
+    pub_mir = rospy.Publisher('tf_pose_mir', geometry_msgs.msg.Pose,queue_size=1)
 
-    rate = rospy.Rate(10.0)
+    rate = rospy.Rate(20.0)
     while not rospy.is_shutdown():
         try:
-            listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(4.0))
+            # listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(4.0))
             (trans,rot) = listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
+            pose = MyPose(trans, rot)
+            pub.publish(pose)
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-            continue
+            pass
+        try:
+            # listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(4.0))
+            (trans_mir,rot_mir) = listener.lookupTransform(target_frame_mir, source_frame_mir, rospy.Time(0))
+            pose_mir = MyPose(trans_mir, rot_mir)
+            pub_mir.publish(pose_mir)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+            pass
 
-        pose = MyPose(trans, rot)
-        pub.publish(pose)
+
 
         rate.sleep()

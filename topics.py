4161aad4d48c8721dc3780a2001e9c9f08639e3e
TimmerAlexander <timmer.alexander@hotmail.com>
2022-11-22 12:09:00 +0100

    added possibility for custom topic_names

--- topics.py
+++ topics.py
@@ -7,17 +7,22 @@ from typing import List
 
 class PosePublisher():
 
-    def __init__(self, target_frames: List[str], source_frames: List[str], rate: float = 10.0):
+    def __init__(self, target_frames: List[str], source_frames: List[str], rate: float = 10.0, pub_topics: List[str] = None):
         # check if node is already running
         if rospy.get_name() == "/unnamed":
             rospy.init_node("pose_publisher")
         if len(target_frames) != len(source_frames):
             raise ValueError("target_frames and source_frames must have the same length")
+        if len(pub_topics) != len(target_frames):
+            raise ValueError("pub_topics must have the same length as target_frames")
         self.target_frames = target_frames
         self.source_frames = source_frames
-        self.pubs = [rospy.Publisher(f'/tf_pose_{source}_TO_{target}', geometry_msgs.msg.Pose, queue_size=10) for source, target in zip(source_frames, target_frames)]
+        
+        if pub_topics is None:
+            pub_topics = [f"/tf_pose_{source}_TO_{target}" for source, target in zip(source_frames, target_frames)]
+        self.pubs = [rospy.Publisher(topic, geometry_msgs.msg.Pose, queue_size=10) for topic in pub_topics]
         self.listener = tf.TransformListener()
-        self.rate = rospy.Rate(10.0)
+        self.rate = rospy.Rate(rate)
 
     def publish_poses(self):
         while not rospy.is_shutdown():

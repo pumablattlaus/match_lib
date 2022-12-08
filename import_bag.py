8cd7e6704e107e8a7556395a103d623c3f3e8bd6
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-12-08 11:26:35 +0100

    func to get joint_pos and vel

--- import_bag.py
+++ import_bag.py
@@ -1,9 +1,9 @@
-from typing import List, Callable, Tuple
+from typing import List, Callable, Tuple, Optional
 import rospy
 import rosbag
 import os
 
-from match_lib.match_geometry import MyPose
+from .match_geometry import MyPose
 # from match_geometry import MyPose
 from scipy.spatial.transform import Rotation
 import cv2 as cv
@@ -80,6 +80,25 @@ def write_twists(bag_file_name, topic_name: str = "/mur/velocity_command", out_f
         for t, cmd in zip(times, twists):
             fd.write(t.__str__()+";"+cmd.linear.__reduce__()[2].__str__()[1:-1] + ";" + cmd.angular.__reduce__()[2].__str__()[1:-1]+"\n")
 
+def get_joint_states(bag_file_name, topic_name: str = "/mur/ur/joint_states", joint_names: Optional[List[str]] = None):
+    joint_positions = []
+    joint_velocities = []
+    times = []
+    if joint_names is None:
+            joint_names = ['UR16/shoulder_pan_joint', 'UR16/shoulder_lift_joint', 'UR16/elbow_joint', 'UR16/wrist_1_joint', 'UR16/wrist_2_joint', 'UR16/wrist_3_joint']
+    else:
+        joint_names = joint_names
+    for _, msg, t in rosbag.Bag(bag_file_name).read_messages(topics=topic_name):
+        q = np.zeros(len(joint_names))
+        q_dot = np.zeros(len(joint_names))
+        times.append(t)
+        for i in range(len(joint_names)):
+            idx=msg.name.index(joint_names[i])
+            q[i] = msg.position[idx]
+            q_dot[i] = msg.velocity[idx]
+        joint_positions.append(q)
+        joint_velocities.append(q_dot)
+    return times, joint_positions, joint_velocities
 
 if __name__ == "__main__":
     path="/home/hauke/Documents/WindowsDocuments/"

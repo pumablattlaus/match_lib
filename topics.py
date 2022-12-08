99596de3ae04b74c2d30c6f083887c2332957887
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-12-08 11:30:49 +0100

    different possibilities to transform Vector (tf doesnt work if frame is moving? (0 pos for mir in ur_frame))

--- topics.py
+++ topics.py
@@ -2,8 +2,8 @@
 import rospy
 import tf
 import geometry_msgs.msg
-from geometry_msgs.msg import Pose, Vector3Stamped
-from match_lib.match_geometry import MyPose
+from geometry_msgs.msg import Pose, Vector3Stamped, QuaternionStamped
+from .match_geometry import MyPose, rotateVector
 from typing import List
 
 class PosePublisher():
@@ -66,19 +66,29 @@ class RotatePosePublisher():
         self.sub = rospy.Subscriber(topic_sub, Pose, self.callback)
         self.vector = Vector3Stamped()
         self.vector.header.frame_id = self.source_frame
+        self.orient = QuaternionStamped()
+        self.orient.header.frame_id = self.source_frame
+
 
 
     def callback(self, msg: Pose):
         if msg is None:
             return
         self.vector.header.stamp = rospy.Time()
+        self.orient.header.stamp = rospy.Time()
 
-        msg.position = self.rotate_xyz(msg.position)
-        msg.orientation = self.rotate_xyz(msg.orientation)
+        pos = self.transform_vec(msg.position)
+        rot = self.transform_orientation(msg.orientation)
+        if pos is None or rot is None:
+            return
+        msg.position = pos
+        # msg.orientation = rot
         self.pub.publish(msg)
 
 
-    def rotate_xyz(self, msg):
+    def transform_vec(self, msg):
+        """doesnt work if frame is also moving?!
+        """
         self.vector.vector.x = msg.x
         self.vector.vector.y = msg.y
         self.vector.vector.z = msg.z
@@ -89,23 +99,48 @@ class RotatePosePublisher():
             return
         msg.x, msg.y, msg.z = vector_out.vector.x, vector_out.vector.y, vector_out.vector.z
         return msg
+    
+    def transform_orientation(self,msg):
+        self.orient.quaternion.x = msg.x
+        self.orient.quaternion.y = msg.y
+        self.orient.quaternion.z = msg.z
+        self.orient.quaternion.w = msg.w
+        try:
+            out = self.listener.transformQuaternion(self.target_frame, self.orient)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+            rospy.loginfo(e)
+            return
+        msg.x, msg.y, msg.z, msg.w = out.quaternion.x, out.quaternion.y, out.quaternion.z, out.quaternion.w
+        return msg
 
+    def rotate_xyz_by_match_geometry(self, msg):
+        try:
+            _,r = self.listener.lookupTransform(self.target_frame, self.source_frame, rospy.Time(0))
+            # transform = self.listener.fromTranslationRotation((0,0,0),r)        
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+            rospy.loginfo(e)
+            return
+        p=rotateVector((msg.x, msg.y, msg.z, 1.0), r, transpose=False)
+        msg.x, msg.y, msg.z = p.x, p.y, p.z
+        return msg
         
 
 if __name__ == '__main__':
     # source_frame = 'map'
-    # rospy.init_node('tf_listener')
+    rospy.init_node('tf_listener')
 
     rospy.set_param('/use_sim_time', True)
 
-    target_frame = 'mur/ur/wrist_3_link'
-    source_frame = 'mur/ur/base_link'
-    source_frame_mir = "mur/mir/base_footprint"
-    target_frame_mir = "map"
-    source_frames = [source_frame, source_frame_mir]
-    target_frames = [target_frame, target_frame_mir]
+    # target_frame = 'mur/ur/wrist_3_link'
+    # source_frame = 'mur/ur/base_link'
+    # source_frame_mir = "mur/mir/base_footprint"
+    # target_frame_mir = "map"
+    # source_frames = [source_frame, source_frame_mir]
+    # target_frames = [target_frame, target_frame_mir]
 
-    pose_publisher = PosePublisher(target_frames, source_frames)
-    rotPos = RotatePosePublisher("/map", pose_publisher.source_frames[0], pose_publisher.pubs[0].name, "/mur/ur/target_pose_rotated")
-    pose_publisher.publish_poses()
+    # pose_publisher = PosePublisher(target_frames, source_frames)
+    # rotPos = RotatePosePublisher("/map", pose_publisher.source_frames[0], pose_publisher.pubs[0].name, "/mur/ur/target_pose_rotated")
+    # pose_publisher.publish_poses()
 
+    RotatePosePublisher("/mur/ur/base_link","/map", "/ur_base_to_map", "/ur_base_to_map_in_base_rot", tf_timeout=3)
+    rospy.spin()
